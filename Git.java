42265e777796991465fb494462503b8f3847938c
Markitoh <51713413+Markitoh@users.noreply.github.com>
2019-06-12 11:00:02 -0400

    Add files via upload

---
+++ Git.java
@@ -0,0 +1,29 @@
+public class Git {
+
+public static void Publicar(String Nombre, String Apellido, int Edad, String trabajo) {
+	
+	/*Nombre = "";
+	Apellido = "";
+	Edad = 0;
+	trabajo = "T";
+	*/
+	
+	System.out.println("Mi nombre es"+" "+ Nombre +" " + Apellido);
+	System.out.println("Tengo "+ Edad + " años de edad");
+	System.out.println("Trabajo en " + trabajo);
+			
+		
+	}
+	
+		
+	public static void main (String [] args) {
+		// TODO Auto-generated method stub
+
+	Publicar("Marco", "Brujan", 45, "NT");
+		
+	}
+	
+	
+	
+
+}
